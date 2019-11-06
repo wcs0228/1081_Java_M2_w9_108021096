a4754631b747d0f9e8d42e443d62d05a1342e0eb
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-06 16:04:03 +0800

    w9

---
+++ src/M2Q17.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+    public class M2Q17 {
+
+    public static void main(String[] args) {
+	Scanner scn= new Scanner(System.in);
+	long n = scn.nextLong();
+	int sum =0;
+	int k =0;
+	while(n>0)
+    {
+        k++;
+        sum=sum+(int)n%10;
+        n=n/10;
+    }
+        System.out.println(k);
+        System.out.println(sum);
+    }
+}
